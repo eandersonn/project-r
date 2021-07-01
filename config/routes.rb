07c74218d524ee861c9d94fb83fc0549a2da2067
Erin Anderson <e.anderson1226@gmail.com>
2021-07-01 08:54:29 -0400

    Adding the welcome page route as well as the signup page for patients when they first signup to the website andon't have an account.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+  #home page
+  get '/' => 'sessions#welcome'
+
+  #singup
+  get '/signup' => 'patients#new'
+  post '/signup' => 'patients#create'
+
+  #login
+
+  #logout
+
+  resources :appointments
+  resources :categories
+  resources :doctors
+  resources :patients
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
